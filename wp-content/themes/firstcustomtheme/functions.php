4704468994af37a73c61803c0d98fcde1bc195af
arebrinkemil <earebrink@gmail.com>
2024-03-17 16:53:03 +0000

    first steps

--- wp-content/themes/firstcustomtheme/functions.php
+++ wp-content/themes/firstcustomtheme/functions.php
@@ -1,5 +1,29 @@
 <?php
 
+
+function customtheme_theme_support()
+{
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array('search-form'));
+}
+
+add_action('after_setup_theme', 'customtheme_theme_support');
+
+
+function customtheme_menus()
+{
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'customtheme_menus');
+
 function customtheme_register_styles()
 {
 
@@ -11,3 +35,18 @@ function customtheme_register_styles()
 
 
 add_action('wp_enqueue_scripts', 'customtheme_register_styles');
+
+
+
+function customtheme_register_scripts()
+{
+
+    wp_enqueue_script('customtheme_jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', true);
+    wp_enqueue_script('customtheme_popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0', true);
+    wp_enqueue_script('customtheme_bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', true);
+    wp_enqueue_script('customtheme_main', get_template_directory_uri() . "/assets/javascript/main.js", array(), '1.0.0', true);
+};
+
+
+
+add_action('wp_enqueue_scripts', 'customtheme_register_scripts');
